5deefa7cde80b762e8263565d0ad31028660b2e8
WhiteDevils24 <akbar240803@gmail.com>
2024-06-06 20:28:58 +0700

    Change Multplayer and Singleplayer

--- Assets/Code/SceneSetting.cs
+++ Assets/Code/SceneSetting.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneSetting : MonoBehaviour
 {
+    //Start Scene Script
     public void ChoosePlayerScene()
     {
         SceneManager.LoadScene("ChoosePlayer");
@@ -14,5 +15,17 @@ public class SceneSetting : MonoBehaviour
         UnityEngine.Debug.Log("Exit Game");
         Application.Quit();
     }
+
+    //Choose Mode Player
+    public void SinglePlayerMode()
+    {
+        SceneManager.LoadScene("SinglePlayerScene");
+    }
+
+    public void MultiPlayerMode()
+    {
+        SceneManager.LoadScene("MultiPlayerScene");
+    }
+
 
 }
